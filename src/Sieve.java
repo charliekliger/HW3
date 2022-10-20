cd2250dd43ec7e436323cf71a47e15f2c2c87641
charliekliger <charliejae16@gmail.com>
2022-10-19 21:21:34 -0400

    Initial commit

---
+++ src/Sieve.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.util.Scanner;
+
+public class Sieve
+{
+    public int primesTo(int n)
+    {
+        LinkedQueue<Integer> numbers = new LinkedQueue<>();
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter upper bound");
+        n = sc.nextInt();
+
+        for (int i = 2; i <= n; i++)
+        {
+
+
+        }
+        return n;
+    }
+}
