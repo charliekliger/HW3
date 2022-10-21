899bba17fe0def85a6971785858e5223b1ec2fb0
charliekliger <charliejae16@gmail.com>
2022-10-21 18:12:48 -0400

    Continued algorithm for primesTo method.

--- src/Queue/Queue.java
+++ src/Queue/Queue.java
@@ -1,4 +1,4 @@
-import Queue.Queue;
+package Queue;
 
 /*
  * Copyright 2014, Michael T. Goodrich, Roberto Tamassia, Michael H. Goldwasser
@@ -25,64 +25,43 @@ import Queue.Queue;
 
 //package net.datastructures;
 
-
 /**
- * Realization of a FIFO queue as an adaptation of a SinglyLinkedList.
- * All operations are performed in constant time.
+ * Interface for a queue: a collection of elements that are inserted
+ * and removed according to the first-in first-out principle. Although
+ * similar in purpose, this interface differs from java.util.Queue.
  *
  * @author Michael T. Goodrich
  * @author Roberto Tamassia
  * @author Michael H. Goldwasser
- * @see SinglyLinkedList
  */
-public class LinkedQueue<E> implements Queue<E>
-{
-
-    /** The primary storage for elements of the queue */
-    private SinglyLinkedList<E> list = new SinglyLinkedList<>();   // an empty  list
-
-    /** Constructs an initially empty queue. */
-    public LinkedQueue() { }                  // new queue relies on the initially empty list
-
+public interface Queue<E> {
     /**
      * Returns the number of elements in the queue.
      * @return number of elements in the queue
      */
-    @Override
-    public int size() { return list.size(); }
+    int size();
 
     /**
      * Tests whether the queue is empty.
      * @return true if the queue is empty, false otherwise
      */
-    @Override
-    public boolean isEmpty() { return list.isEmpty(); }
+    boolean isEmpty();
 
     /**
      * Inserts an element at the rear of the queue.
-     * @param element  the element to be inserted
+     * @param e  the element to be inserted
      */
-    @Override
-    public void enqueue(E element) { list.addLast(element); }
+    void enqueue(E e);
 
     /**
      * Returns, but does not remove, the first element of the queue.
      * @return the first element of the queue (or null if empty)
      */
-    @Override
-    public E first() { return list.first(); }
+    E first();
 
     /**
      * Removes and returns the first element of the queue.
      * @return element removed (or null if empty)
      */
-    @Override
-    public E dequeue() { return list.removeFirst(); }
-
-    /** Produces a string representation of the contents of the queue.
-     *  (from front to back). This exists for debugging purposes only.
-     */
-    public String toString() {
-        return list.toString();
-    }
+    E dequeue();
 }
--- src/Sieve.java
+++ src/Sieve.java
@@ -1,9 +1,11 @@
 import java.util.*;
 import java.util.Scanner;
+import java.lang.Math;
+
 
 public class Sieve
 {
-    public int primesTo(int n)
+    public static int primesTo(int n)
     {
         LinkedQueue<Integer> numbers = new LinkedQueue<>();
 
@@ -11,11 +13,41 @@ public class Sieve
         System.out.println("Please enter upper bound");
         n = sc.nextInt();
 
+        if(n < 2)
+        {
+            System.err.println("Error: Input must be a number greater than 2.");
+            n = sc.nextInt();
+        }
+
         for (int i = 2; i <= n; i++)
         {
+            numbers.enqueue(i);
+        }
 
+        LinkedQueue<Integer> primes = new LinkedQueue<>();
 
+        while (!numbers.isEmpty())
+        {
+            int p = numbers.dequeue();
+
+            while (p <= Math.sqrt(n))
+            {
+                primes.enqueue(p);
+
+                /*
+                    for (number : numbers)
+                        if (number % p == 0)
+                            numbers.dequeue()
+
+                */
+
+            }
         }
         return n;
+    }
+
+    public static void main(String[] args)
+    {
+        primesTo(10);
     }
 }
