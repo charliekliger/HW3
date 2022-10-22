03b61bf49db01a0cad01331eb32350fc999c283a
charliekliger <charliejae16@gmail.com>
2022-10-22 16:10:54 -0400

    Continued algorithm for primesTo method.

--- src/Sieve.java
+++ src/Sieve.java
@@ -1,53 +1,58 @@
-import java.util.*;
 import java.util.Scanner;
 import java.lang.Math;
 
 
 public class Sieve
 {
-    public static int primesTo(int n)
+    public static void primesTo()
     {
         LinkedQueue<Integer> numbers = new LinkedQueue<>();
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Please enter upper bound");
-        n = sc.nextInt();
+        int n = sc.nextInt();
 
+        // throw exception for invalid input
         if(n < 2)
         {
             System.err.println("Error: Input must be a number greater than 2.");
             n = sc.nextInt();
         }
 
+        // add all numbers from 2 to n to numbers
         for (int i = 2; i <= n; i++)
         {
             numbers.enqueue(i);
         }
 
-        LinkedQueue<Integer> primes = new LinkedQueue<>();
 
-        while (!numbers.isEmpty())
+        LinkedQueue<Integer> primes = new LinkedQueue<>();
+        LinkedQueue<Integer> maybePrime = new LinkedQueue<>();
+
+        while (!(numbers.isEmpty()))
         {
             int p = numbers.dequeue();
 
-            while (p <= Math.sqrt(n))
+            while (p <= (int) Math.sqrt(n))
             {
                 primes.enqueue(p);
 
-                /*
-                    for (number : numbers)
-                        if (number % p == 0)
-                            numbers.dequeue()
+                while (!(numbers.isEmpty()))
+                {
+                    int num = numbers.dequeue();
+                    if (!(num % p == 0))
+                        maybePrime.enqueue(num);
+                }
 
-                */
 
             }
+            primes.enqueue(p);
         }
-        return n;
+
     }
 
     public static void main(String[] args)
     {
-        primesTo(10);
+        primesTo();
     }
 }
